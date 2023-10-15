d280a0a68f92856ef76a5cb36861e56fa0fc1970
Priscilla Hurtado <PriscillaHM>
2023-10-14 23:31:01 -0700

    Se agrego la parte donde busca las coincidencias con el archivo de tokens y falta que regrese los id de cada token

--- parser.js
+++ parser.js
@@ -1,4 +1,6 @@
 const fs = require('fs')
+const coincidencias = [];
+const palabras = [];
 
 fs.readFile('sql.txt', 'utf-8', (err, data) => {
     if (err) {
@@ -6,7 +8,7 @@ fs.readFile('sql.txt', 'utf-8', (err, data) => {
     }
 
     const simbolos = ['.', ',', '-', ' ', ';', '*', '(', ')', ':', '\n', '\r']; 
-    const palabras = [];
+    
     let palabraActual = "";
 
     for (let i = 0; i < data.length; i++) {
@@ -21,4 +23,33 @@ fs.readFile('sql.txt', 'utf-8', (err, data) => {
         }
     }
     console.log(palabras);
+});
+
+// COINCIDENCIAS
+fs.readFile('sqlkeywords.txt', 'utf8', (err, data) => {
+  if (err) {
+    console.error('Error al leer el archivo de texto:', err);
+    return;
+  }
+
+  const lineas = data.split('\n'); // Dividir el texto en líneas
+
+  // Iterar sobre las líneas del archivo
+  lineas.forEach((linea) => {
+    const palabra = linea.trim(); // Eliminar espacios en blanco alrededor de la palabra
+
+    // Verificar si la palabra coincide con alguna en el arreglo
+    if (palabras.includes(palabra)) {
+      coincidencias.push(palabra);
+    }
+  });
+
+  if (coincidencias.length > 0) {
+    console.log('Palabras coincidentes:');
+    coincidencias.forEach(palabra => {
+      console.log(palabra);
+    });
+  } else {
+    console.log('No se encontraron palabras coincidentes.');
+  }
 });
