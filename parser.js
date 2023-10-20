4ed507b3f5cb894679c6a41a0391ddd14d35511b
Priscilla Hurtado <PriscillaHM>
2023-10-19 19:44:24 -0700

    Se asignan los tokens e imprime sus id, falta añadir limitaciones sobre las palabras no reservadas

--- parser.js
+++ parser.js
@@ -1,55 +1,43 @@
 const fs = require('fs')
-const coincidencias = [];
+const simbolos = ['.', ',', '-', ' ', ';', '*', '(', ')', ':', "", '', '\n', '\r']; 
 const palabras = [];
+let palabraActual = "";
 
 fs.readFile('sql.txt', 'utf-8', (err, data) => {
-    if (err) {
-        console.log(err);
-    }
-
-    const simbolos = ['.', ',', '-', ' ', ';', '*', '(', ')', ':', '\n', '\r']; 
-    
-    let palabraActual = "";
-
-    for (let i = 0; i < data.length; i++) {
-        if (simbolos.includes(data[i])) {
-            if (palabraActual.length > 0) {
-                palabras.push(palabraActual);
-            }
-            palabras.push(data[i]);
-            palabraActual = ""; 
-        } else {
-            palabraActual += data[i];
-        }
-    }
-    console.log(palabras);
+  if (err) console.log(err);
+  for (let i = 0; i < data.length - 1; i++) {
+      if (simbolos.includes(data[i])) {
+          if (palabraActual.length > 0) {
+              if ( data[i] == "." && data[i + 1].match(/[a-zA-Z]/) ) {
+                  palabraActual += data[i];
+              } else {
+                  palabras.push(palabraActual);
+              }
+              palabras.push(data[i]);
+              palabraActual = ""; 
+          } else {
+              palabras.push(data[i]);
+          }
+      } else {
+          palabraActual += data[i];
+      }
+  }
+    //console.log(palabras);
 });
 
 // COINCIDENCIAS
-fs.readFile('sqlkeywords.txt', 'utf8', (err, data) => {
+fs.readFile('sqlkeywords.txt', 'utf-8', (err, data) => {
+  /* console.log(palabras); */
   if (err) {
-    console.error('Error al leer el archivo de texto:', err);
-    return;
+      console.log(err);
   }
-
-  const lineas = data.split('\n'); // Dividir el texto en líneas
-
-  // Iterar sobre las líneas del archivo
-  lineas.forEach((linea) => {
-    const palabra = linea.trim(); // Eliminar espacios en blanco alrededor de la palabra
-
-    // Verificar si la palabra coincide con alguna en el arreglo
-    if (palabras.includes(palabra)) {
-      coincidencias.push(palabra);
-    }
-  });
-
-  if (coincidencias.length > 0) {
-    console.log('Palabras coincidentes:');
-    coincidencias.forEach(palabra => {
-      console.log(palabra);
-    });
-  } else {
-    console.log('No se encontraron palabras coincidentes.');
+  const keywords = data.split('\n');
+  for (let i = 0; i < palabras.length; i++) {
+      for (let j = 0; j < keywords.length; j++) {
+          let [id,  , valor] = keywords[j].split(' ');
+          if (palabras[i] == valor) {
+              console.log(id);
+          }
+      }   
   }
 });
