30568685a54ecf5e17cf7a18ca01de2a5a678142
Priscilla Hurtado <PriscillaHM>
2023-11-01 15:29:05 -0700

    Se modifico la parte donde se hace el arrelo de palabras a las cuales se les dara un token

--- parser.js
+++ parser.js
@@ -1,30 +1,24 @@
 const fs = require('fs')
-const simbolos = ['.', ',', '-', ' ', ';', '*', '(', ')', ':', "", '', '\n', '\r']; 
+const simbolos = ['.', ',', '-', ' ', ';', '*', '(', ')', ':', "", '', '\n', '\r', "'", '"']; 
 const palabras = [];
 let palabraActual = "";
 
 fs.readFile('sql.txt', 'utf-8', (err, data) => {
-  if (err) console.log(err);
-  for (let i = 0; i < data.length - 1; i++) {
-      if (simbolos.includes(data[i])) {
-          if (palabraActual.length > 0) {
-              if ( data[i] == "." && data[i + 1].match(/[a-zA-Z]/) ) {
-                  palabraActual += data[i];
-              } else {
-                  palabras.push(palabraActual);
-              }
-              palabras.push(data[i]);
-              palabraActual = ""; 
-          } else {
-              palabras.push(data[i]);
-          }
-      } else {
-          palabraActual += data[i];
-      }
-  }
-    //console.log(palabras);
-});
+    if (err) console.log(err);
 
+    for (let i = 0; i < data.length; i++) {
+        if (simbolos.includes(data[i])) {
+            if (palabraActual.length > 0) {
+                palabras.push(palabraActual);
+                palabraActual = ""; 
+            } else {
+                palabras.push(data[i]);
+            }
+        } else {
+            palabraActual += data[i];
+        }
+    }
+});
 // COINCIDENCIAS
 fs.readFile('sqlkeywords.txt', 'utf-8', (err, data) => {
   console.log(palabras);
